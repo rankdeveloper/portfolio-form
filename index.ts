face26c97938fd85fc56e747c549d9de6b3f9de6
Rankush <iamrankush@gmail.com>
2025-06-29 18:38:29 +0530

    cors error fix

--- index.ts
+++ index.ts
@@ -3,12 +3,15 @@ const app = express();
 require("dotenv").config();
 const messageRouter = require("./routes/message");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use("/message", messageRouter);
 
